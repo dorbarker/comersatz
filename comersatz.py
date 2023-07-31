7db9f22c518a1ad8808ef93aecad97bd5b37f408
Dillon Barker <dor.barker@gmail.com>
2023-07-31 14:13:48 -0500

    filter out unpaired illumina reads

--- comersatz.py
+++ comersatz.py
@@ -50,10 +50,14 @@ def calculate_required_read_count(total: int, proportion_of_total: float) -> int
     return math.floor(total * proportion_of_total) or 1
 
 
-def sample_reads(reads: Path, required_reads: int, seed: int = 11) -> str:
+def sample_reads(reads: Path, required_reads: int, paired_read_names: list[str], seed: int = 11) -> str:
     approximate_total_reads = estimate_read_count(reads)
 
     proportion_required = min(1.0, (required_reads / approximate_total_reads) * 1.5)
+
+    grep_cmd = ("seqkit", "grep", "-f", "-", reads)
+
+    grepped_reads = subprocess.run(grep_cmd, capture_output=True, text=True, input="\n".join(paired_read_names))
 
     sample_cmd = (
         "seqkit",
@@ -61,11 +65,10 @@ def sample_reads(reads: Path, required_reads: int, seed: int = 11) -> str:
         "--rand-seed",
         str(seed),
         "-p",
-        str(proportion_required),
-        reads,
+        str(proportion_required)
     )
 
-    sampled = subprocess.run(sample_cmd, capture_output=True, text=True)
+    sampled = subprocess.run(sample_cmd, capture_output=True, text=True, input=grepped_reads)
 
     head_cmd = ("seqkit", "head", "-n", str(required_reads))
 
@@ -83,6 +86,18 @@ def shuffle_reads(reads: str, seed: int = 11) -> str:
 
     return shuffled.stdout
 
+def get_paired_read_names(fwd: Path, rev: Path) -> list[str]:
+    
+    fwd_names = get_read_names(fwd)
+    rev_names = get_read_names(rev)
+
+    return sorted(set(fwd_names).intersection(set(rev_names)))
+
+def get_read_names(reads: Path) -> list[str]:
+
+    cmd = ("seqkit", "--name", "--only-id", reads)
+
+    return subprocess.run(cmd, text=True, capture_output=True).stdout.splitlines()
 
 def estimate_read_count(reads: Path, assumption_length: int = 150):
     size_bytes = reads.stat().st_size
@@ -103,6 +118,8 @@ def construct_illumina_metagenome(illumina_triplets, total_output_reads, outdir,
 
     for fwd, rev, prob in illumina_triplets:
         desired_reads = calculate_required_read_count(total_output_reads, prob)
+
+        paired_read_names = get_paired_read_names(fwd, rev)
 
         out_fwd_reads.append(sample_reads(fwd, desired_reads, seed))
         out_rev_reads.append(sample_reads(rev, desired_reads, seed))
